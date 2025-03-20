018186d92bb90266bfc4863e7ee159d8e17623d6
thecatblake <k222ryousuke@gmail.com>
2025-03-21 07:48:51 +0900

    scala list

---
+++ functional-programming-in-scala/list/ex3-1.scala
@@ -0,0 +1,16 @@
+package myapp
+
+import datastructures.List
+import datastructures.List.*
+
+object Solve {
+    @main def printResult: Unit =
+        println(
+            List(1, 2, 3, 4, 5) match 
+                case Cons(x, Cons(2, Cons(4, _))) => x
+                case Nil => 42
+                case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+                case Cons(h, t) => h + sum(t)
+                case null => 0
+        )
+}
