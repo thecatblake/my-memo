4b5c2f592affd40d6f9f5ac8e66d9033b2d22431
thecatblake <k222ryousuke@gmail.com>
2025-03-28 02:55:12 +0900

    ex2-3

---
+++ understanding-software-dynamics/chapter2/ex2-3.cc
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <mach/mach_time.h>
+
+static const int kIterations = 1000 * 1000000;
+
+int main(int argc, char** argv) {
+    uint64_t startcy, stopcy;
+    uint64_t sum = 0;
+
+    startcy = mach_absolute_time();
+
+    // printf("Timer started\n");
+
+    for (int i = 0; i < kIterations; i++)
+        sum += 1;
+
+    // printf("Timer stopped\n");
+    
+    stopcy = mach_absolute_time();
+    int64_t elapsed = stopcy - startcy;
+    double felapsed = elapsed;
+    printf("%d iterations, %lld cpu cycles. %f iter/cycle\n", kIterations, elapsed, kIterations / felapsed);
+    return 0;
+}
