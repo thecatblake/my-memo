36ec698b82c0a26cd5f432c1b3768e030b2712a9
thecatblake <k222ryousuke@gmail.com>
2025-03-29 19:54:16 +0900

    subtraction

--- programming-bitcoin/scala/field.scala
+++ programming-bitcoin/scala/field.scala
@@ -4,3 +4,13 @@ package bitcoin.field;
 case class FieldElement(val num: Int, val prime: Int):
     if num >= prime | num < 0 then
         sys.error("%d not in field range 0 to %d" format (num, prime - 1));
+
+    def +(that: FieldElement): FieldElement = 
+        if prime != that.prime then 
+            sys.error("Cannot add two numbers in different fields.");
+        FieldElement((num + that.num) % prime, prime);
+
+    def -(that: FieldElement): FieldElement =
+        if prime != that.prime then 
+            sys.error("Cannot subtract two numbers in different fields.");
+        FieldElement((prime + (num - that.num)) % prime, prime)
--- programming-bitcoin/scala/field_test.scala
+++ programming-bitcoin/scala/field_test.scala
@@ -6,4 +6,11 @@ object Test {
     @main def printResult: Unit =
         println(FieldElement(7, 13) == FieldElement(7, 13))
         println(FieldElement(7, 13) != FieldElement(6, 13))
+
+    @main def testAddition: Unit =
+        println(FieldElement(44, 57) + FieldElement(33, 57))
+
+    @main def testSubtraction: Unit =
+        println(FieldElement(9, 57) - FieldElement(29, 57))
+
 }
